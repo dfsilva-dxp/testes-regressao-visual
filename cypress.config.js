62a9315e53852c16b260047aca62cb4941078521
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-17 19:59:23 -0300

    chore: este commit irá instalar e adicionar as configurações do cypress com percy.

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/",
+  },
+};
