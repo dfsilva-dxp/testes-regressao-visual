d826cb14d3f28a1da3774b08d7eb6769f2603893
Daniel Silva <dfsilva.dxp@gmail.com>
2023-04-18 09:52:38 -0300

    test: este commit irá adicionar os testes de estados da aplicação

--- cypress/e2e/ticketbox.spec.cy.js
+++ cypress/e2e/ticketbox.spec.cy.js
@@ -4,4 +4,25 @@ describe("template spec", () => {
   it("Check for the initial state", () => {
     cy.percySnapshot();
   });
+
+  it("Checks for invalid email", () => {
+    cy.get("#email").type("dfsilva&email.com");
+    cy.percySnapshot();
+  });
+
+  it("Enables submission after all mandatory fields are filled", () => {
+    cy.get("#first-name").type("Daniel");
+    cy.get("#last-name").type("Silva");
+    cy.get("#email").type("dfsilva@email.com");
+    cy.get("#agree").check();
+    cy.percySnapshot();
+  });
+
+  it("Update agreement base on full name, tickets quantity and type", () => {
+    cy.get("#first-name").type("Daniel");
+    cy.get("#last-name").type("Silva");
+    cy.get("#ticket-quantity").select("4");
+    cy.get("#vip").check();
+    cy.percySnapshot();
+  });
 });
